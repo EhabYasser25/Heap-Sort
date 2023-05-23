ecda7be86a5f07ed0859109d9fd7d961a4ee964a
dave-nagib <davidnagib101@gmail.com>
2023-05-23 04:37:57 +0300

    Tests start

---
+++ src/main/java/Tests/SortingTest.java
@@ -0,0 +1,12 @@
+package Tests;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.hs.Sorting.*;
+import org.hs.Sort_Array;
+public class SortingTest {
+    /**
+     * Worst-case, average-case, best-case scenarios for each
+     * algorithm (12 tests), and 8 comparison tests on a large array.
+     * All tests will test both correctness and time values.
+     */
+}
